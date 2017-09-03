c74fe09f07c6a2ab05e199ce12df706d53aeae0f
flgor <c7florin@gmail.com>
2017-09-03 23:26:13 +0300

    - implemented plans display - bind(this) ... why??

--- src/main/js/planComponent.js
+++ src/main/js/planComponent.js
@@ -8,26 +8,56 @@ class PlanComponent extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {plans: []};
     }
 
     componentDidMount() {
         client.get('/plan')
             .then(function (response) {
-                console.log(response);
-            })
+                console.log("response: ", response);
+                console.log("data: ", response.data);
+                this.setState({plans: response.data})
+            }.bind(this))
             .catch(function (error) {
                 console.log(error);
             });
-        this.setState({plans: []});
     }
 
     render() {
-        if (this.state.plans === null) {
-            return <div>Loading...</div>
-        } else {
-            return <div> React Plans</div>
-        }
+        return (<table className="table">
+            <thead>
+            <tr>
+                <th>Plan</th>
+                <th>BillingCycle</th>
+                <th>Price</th>
+                <th>Enabled</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <PlanRowsComponent plans={this.state.plans}/>
+        </table>)
+    }
+}
+
+class PlanRowsComponent extends React.Component {
+    render() {
+        console.log("PlanRowsComponent:", this.props.plans);
+        var planRowComponents = this.props.plans.map(
+            plan => <PlanRowComponent key={plan.name} plan={plan}/>
+        );
+        return (<tbody>{planRowComponents}</tbody>);
+    }
+}
+
+class PlanRowComponent extends React.Component {
+    render() {
+        return <tr>
+            <td>{this.props.plan.name}</td>
+            <td>{this.props.plan.billingCycle}</td>
+            <td>{this.props.plan.price}</td>
+            <td>{this.props.plan.enabled}</td>
+            <td>Restrictions/Validate with BT</td>
+        </tr>
     }
 }
 
